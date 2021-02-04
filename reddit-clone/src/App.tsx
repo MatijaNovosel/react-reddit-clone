ec4cec02eb638495a869eef9b59f851db46b7cfb
Matija Novosel <mnovosel5@gmail.com>
2021-02-04 12:20:09 +0100

    Tailwind integration

--- reddit-clone/src/App.tsx
+++ reddit-clone/src/App.tsx
@@ -1,6 +1,8 @@
 function App() {
   return (
-    <h1>Hello world</h1>
+    <div className="p-5 shadow-md bg-gray-700">
+      <span className="text-white">Hello world</span>
+    </div>
   );
 }
 
---
+++ reddit-clone/tailwind.config.js
@@ -0,0 +1,31 @@
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false,
+  theme: {
+    colors: {
+      gray: colors.blueGray,
+      blue: colors.blue,
+      yellow: colors.yellow,
+      red: colors.red,
+      green: colors.green,
+      pink: colors.pink,
+      purple: colors.purple,
+      white: colors.white,
+      orange: colors.orange,
+      black: colors.black
+    },
+    extend: {
+      colors: {
+        yellow: {
+          "450": "#fed001"
+        }
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
