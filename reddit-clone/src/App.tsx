53c5d07a12363be3d1f0d63ffcc70a4b0e546cbe
MatijaNovosel <mnovosel5@gmail.com>
2021-02-23 21:28:24 +0100

    Fixes

--- reddit-clone/src/App.tsx
+++ reddit-clone/src/App.tsx
@@ -3,11 +3,9 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { routes } from "./router/routes";
 
 function AppRouter() {
-  const pathname = window.location.pathname;
-
   return (
     <Router>
-      {pathname === "/login" ? "" : <Navbar />}
+      <Route path="/*" render={ ( props ) => ( props.location.pathname !== "/login") && <Navbar /> }/>
       <Switch>
         {routes.map((route, i) => (
           <Route {...route} key={i} />
